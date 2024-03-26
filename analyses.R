0c4236e9a6d74d985c5168cf7eaf98534d9f74ff
n-murray-psych <85261331+n-murray-psych@users.noreply.github.com>
2024-03-26 19:30:18 -0300

    Plots for DiSP paper done

--- analyses.R
+++ analyses.R
@@ -83,3 +83,100 @@ update(pairs(line_task_ls_interaction), by = NULL, adjust = "holm")
 
 # TESTING VISUALIZATIONS WITH AFEX
 afex_plot(line_task_anova, c("cue_location", "cue_type"), error = "within", return = "data")
+afex_plot(detection_rt_anova, c("cue_type", "cue_validity"), error = "within")
+
+
+illusory_motion <- data.frame(
+  cue_type = c(
+    "Gaze Right", "Gaze Neutral", "Gaze Left", 
+    "Exogenous Right", "Exogenous Neutral", "Exogenous Left"
+  ),
+  estimate = c(
+    0.094, 0.076, 0.097, 
+    -0.133, 0.023, 0.230
+  ),
+  lowci = c(
+    0.012, 0.012, 0.045, 
+    -0.196, -0.007, 0.133
+  ), 
+  highci = c(
+    0.175, 0.140, 0.147, 
+    -0.070, 0.054, 0.325
+  )
+)
+
+rightward_motion <- illusory_motion <- data.frame(
+  cue_type = c(
+    "Gaze Right", "Gaze Neutral", "Gaze Left", 
+    "Exogenous Right", "Exogenous Neutral", "Exogenous Left"
+  ),
+  estimate = c(
+    0.202, 0.213, 0.230, 
+    0.173, 0.10, 0.226
+  ),
+  lowci = c(
+    0.111, 0.154, 0.153, 
+    0.089, 0.044, 0.162 
+  ), 
+  highci = c(
+    0.293, 0.272, 0.306,
+    0.257, 0.153, 0.289
+  )
+)
+
+leftward_motion <- illusory_motion <- data.frame(
+  cue_type = c(
+    "Gaze Right", "Gaze Neutral", "Gaze Left", 
+    "Exogenous Right", "Exogenous Neutral", "Exogenous Left"
+  ),
+  estimate = c(
+    -0.190, -0.140, -0.156, 
+    -0.178, -0.025, -0.060
+  ),
+  lowci = c(
+    -0.276, -0.220, -0.232, 
+    -0.252, -0.115, -0.203
+  ), 
+  highci = c(
+    -0.10, -0.06, -0.08, 
+    -0.10, 0.070, 0.09
+  )
+)
+
+ilm_graph <- function(data, title) {
+  
+    ggplot(
+    data = data,
+    aes(x = estimate, y = cue_type)
+  ) + 
+    geom_point() + 
+    geom_errorbar(aes(xmin = lowci, xmax = highci)) + 
+    labs(title = title) + 
+    xlab("Line Motion Rating (Left to Right") + 
+    ylab("Cue Type") + 
+    geom_vline(xintercept = 0, linetype = "dashed") +
+    xlim(-.5, .5) + 
+    theme_classic()
+  
+}
+
+ilm_graph(illusory_motion, "Illusory Motion")
+ilm_graph(rightward_motion, "Rightward Real Line Motion")
+ilm_graph(leftward_motion, "Leftward Real Line Motion")
+
+
+afex_plot(detection_rt_anova, "cue_validity", error = "within", 
+          trace = "cue_type",
+          data_plot = FALSE,
+          legend_title = "Cue Type"
+          ) + 
+  theme_classic() + 
+  labs(x = "Cue Validity", y = "Reaction Time (ms)")
+  
+afex_plot(detection_accuracy_anova, "cue_validity", error = "within", 
+          trace = "cue_type",
+          data_plot = FALSE,
+          legend_title = "Cue Type"
+          ) + 
+  theme_classic() + 
+  labs(x = "Cue Validity", y = "Reaction Time (ms)")
